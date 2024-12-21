3ce7a4a6fc8e6ea68715022ba06d99f7cab52272
Depo S. H <141189770+dxdeposh@users.noreply.github.com>
2024-12-21 18:20:10 +0700

    nonaktifkan route register admin

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->middleware('guest:admin')->group(function () {
 
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('admin.register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::get('register', [RegisteredUserController::class, 'create'])->name('admin.register');
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [LoginController::class, 'create'])->name('admin.login');
     Route::post('login', [LoginController::class, 'store']);
